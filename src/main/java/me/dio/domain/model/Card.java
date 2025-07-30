6e7b380f00c95491cc93a0570ebce6803764fd5b
Renato dos Santos <rnt.dsantos@gmail.com>
2025-07-30 15:15:19 -0300

    Update Card.java

--- src/main/java/me/dio/domain/model/Card.java
+++ src/main/java/me/dio/domain/model/Card.java
@@ -20,4 +20,31 @@ public class Card {
     @Column(name = "available_limit", precision = 13, scale = 2)
     private BigDecimal limit;
 
+    
+    public Long getId() {
+        return id;
+    }
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    
+    public String getNumber() {
+        return number;
+    }
+    
+    public void setNumber(String number) {
+        this.number = number;
+    }
+    
+    
+    public BigDecimal getLimit() {
+        return limit;
+    }
+    
+    public void setLimit(BigDecimal limit) {
+        this.limit = limit;
+    }
+
 }
