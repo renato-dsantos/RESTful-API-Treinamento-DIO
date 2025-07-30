6801e676c3973420d6b84eb83ebdda7483a4849b
Renato dos Santos <rnt.dsantos@gmail.com>
2025-07-30 15:16:50 -0300

    Update BaseItem.java

--- src/main/java/me/dio/domain/model/BaseItem.java
+++ src/main/java/me/dio/domain/model/BaseItem.java
@@ -15,6 +15,32 @@ public abstract class BaseItem {
 
     private String icon;
 
-    private String description;;
+    private String description;
+
+    public Long getId() {
+    return id;
+    }
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+  
+    public String getIcon() {
+        return icon;
+    }
+    
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+    
+    
+    public String getDescription() {
+        return description;
+    }
+    
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
 }
